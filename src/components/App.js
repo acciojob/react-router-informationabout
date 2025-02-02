7f37d7f03a94e9467d9985c36da596f9721c2cc5
Coder <111493707+informationabout@users.noreply.github.com>
2025-02-02 20:11:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
-
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import './../styles/App.css';
+
+// Home Component
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <p>This is the home page of the application.</p>
+    </div>
+  );
+};
+
+// About Component
+const About = () => {
+  return (
+    <div>
+      <h1>About this Application</h1>
+      <p>This application demonstrates basic routing with React Router.</p>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
 
-export default App
+        {/* Route Matching */}
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
