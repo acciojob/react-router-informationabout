18f73568514363952ad86e9379c561a1c3385589
Coder <111493707+informationabout@users.noreply.github.com>
2025-02-03 21:07:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,48 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "@reach/router";
-import './../styles/App.css';
 
-// Home Component
-const Home = () => {
+import React from "react";
+import './../styles/App.css';
+import { BrowserRouter,Link, Route, Routes } from "react-router-dom";
+
+const Header = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page!</h1>
-      <p>This is the home page of the application.</p>
-    </div>
+    <header>
+      <ul>
+       <li> <Link to="/">Home</Link></li>
+      <li><Link to="/about">About</Link> </li>
+    </ul>
+    </header>
   );
 };
 
-// About Component
+
+const Home = () => {
+  return (
+    <h1>
+     Welcome to my website!
+    </h1>  
+  );
+};
 const About = () => {
   return (
     <div>
-      <h1>About this Application</h1>
-      <p>This application demonstrates basic routing with React Router.</p>
+    <h1>
+     About
+    </h1>
+    <p>This is a sample React Router program.</p>
     </div>
   );
 };
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+   <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
-        {/* Route Matching */}
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
-
-export default App;
+export default App
